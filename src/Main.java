5c68ef8a26a7bef03ae74778be6d20156da62033
Melissa Gomez <melissagomez3024@gmail.com>
2026-02-15 08:24:23 +0100

    Implement sheep counter logic without using else

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,11 @@
 public class Main {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            if (i == 1) {
+                System.out.println(i + " oveja...");
+                continue;
+            }
+            System.out.println(i + " ovejas...");
+        }
+    }
 }
